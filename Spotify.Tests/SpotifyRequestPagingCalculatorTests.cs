c262b6390009bc210c66acc5a37349aa38cdde59
Philip Wong <philip.wong@genesisenergy.co.nz>
2022-04-12 18:13:50 +1200

    Clean up tests

--- Spotify.Tests/SpotifyRequestPagingCalculatorTests.cs
+++ Spotify.Tests/SpotifyRequestPagingCalculatorTests.cs
@@ -16,62 +16,62 @@ namespace Spotify.Tests
             _pagingCalculator = new SpotifyRequestPagingCalculator();
         }
 
-
-        private const string NextUrlWithoutParams = "https://api.spotify.com/v1/playlists/7fq8B138ulrc1KUSbc30r9/tracks";
-        private readonly SpotifyItemResponse<SongItem> _noPagesNeededResponse = new SpotifyItemResponse<SongItem>
-        {
-            items = new SongItem[12].ToList(),
-            next = null,
-            limit = 100,
-            total = 12
-        };
-        private readonly SpotifyItemResponse<SongItem> _onePageNeeded = new SpotifyItemResponse<SongItem>
-        {
-            items = new SongItem[100].ToList(),
-            next = NextUrlWithoutParams + "?offset=100&limit=100",
-            limit = 100,
-            total = 120
-        };
-        private readonly SpotifyItemResponse<SongItem> _fourPagesNeeded = new SpotifyItemResponse<SongItem>
-        {
-            items = new SongItem[100].ToList(),
-            next = NextUrlWithoutParams + "?offset=100&limit=100",
-            limit = 100,
-            total = 420
-        };
-        private readonly IReadOnlyList<string> _expectedOnePageUrl = new List<string>
-        {
-            NextUrlWithoutParams + "?offset=100&limit=100"
-        };
-        private readonly IReadOnlyList<string> _expectedFourPageUrls = new List<string>
-        {
-            NextUrlWithoutParams + "?offset=100&limit=100",
-            NextUrlWithoutParams + "?offset=200&limit=100",
-            NextUrlWithoutParams + "?offset=300&limit=100",
-            NextUrlWithoutParams + "?offset=400&limit=100"
-        };
-
         [Fact]
         public void GetRemainingUrls_GivenNoNextUrl_ThenReturnNoUrls()
         {
-            var urls = _pagingCalculator.GetRemainingUrls(_noPagesNeededResponse);
+            var urls = _pagingCalculator.GetRemainingUrls(_nullNextResponse);
             urls.Should().BeEmpty();
         }
 
         [Fact]
         public void GetRemainingUrls_GivenOnePageNeeded_ThenReturnOneUrl()
         {
-            var urls = _pagingCalculator.GetRemainingUrls(_onePageNeeded);
+            var urls = _pagingCalculator.GetRemainingUrls(_120Total100LimitResponse);
             urls.Should().HaveCount(1);
-            urls.Should().BeEquivalentTo(_expectedOnePageUrl, options => options.WithStrictOrdering());
+            urls.Should().BeEquivalentTo(_expectedUrlsOnePage, options => options.WithStrictOrdering());
         }
 
         [Fact]
         public void GetRemainingUrls_GivenFourPagesNeeded_ThenReturnFourUrls()
         {
-            var urls = _pagingCalculator.GetRemainingUrls(_fourPagesNeeded);
+            var urls = _pagingCalculator.GetRemainingUrls(_220Total50LimitResponse);
             urls.Should().HaveCount(4);
-            urls.Should().BeEquivalentTo(_expectedFourPageUrls, options => options.WithStrictOrdering());
+            urls.Should().BeEquivalentTo(_expectedUrlsFourPages, options => options.WithStrictOrdering());
         }
+
+
+        private const string NextUrlWithoutParams = "https://api.spotify.com/v1/playlists/7fq8B138ulrc1KUSbc30r9/tracks";
+        private readonly SpotifyItemResponse<SongItem> _nullNextResponse = new SpotifyItemResponse<SongItem>
+        {
+            items = new SongItem[12].ToList(),
+            next = null,
+            limit = 100,
+            total = 12
+        };
+        private readonly SpotifyItemResponse<SongItem> _120Total100LimitResponse = new SpotifyItemResponse<SongItem>
+        {
+            items = new SongItem[100].ToList(),
+            next = NextUrlWithoutParams + "?offset=100&limit=100",
+            limit = 100,
+            total = 120
+        };
+        private readonly SpotifyItemResponse<SongItem> _220Total50LimitResponse = new SpotifyItemResponse<SongItem>
+        {
+            items = new SongItem[50].ToList(),
+            next = NextUrlWithoutParams + "?offset=50&limit=50",
+            limit = 50,
+            total = 220
+        };
+        private readonly IReadOnlyList<string> _expectedUrlsOnePage = new List<string>
+        {
+            NextUrlWithoutParams + "?offset=100&limit=100"
+        };
+        private readonly IReadOnlyList<string> _expectedUrlsFourPages = new List<string>
+        {
+            NextUrlWithoutParams + "?offset=50&limit=50",
+            NextUrlWithoutParams + "?offset=100&limit=50",
+            NextUrlWithoutParams + "?offset=150&limit=50",
+            NextUrlWithoutParams + "?offset=200&limit=50"
+        };
     }
 }
